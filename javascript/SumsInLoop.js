0dd4e6ddb438211c1ab768075f7397dd7fbc39ee
DESKTOP-85TAR51\james <jamesherrero@gmail.com>
2018-05-04 19:17:04 +1000

    fixed pathing for readme file

--- javascript/SumsInLoop.js
+++ javascript/SumsInLoop.js
@@ -1,26 +1,25 @@
-fs = require("fs");
+var fs = require("fs");
+var path = require("path");
+
+pathing = path.join(__dirname, "..", "SumsInLoopInput.txt");
 
 var input = [];
 
 // Read input file & splice out the white spaces & breaks
-fs.readFile(
-  "C:\\Users\\james\\Documents\\CodeAbbey\\SumsInLoopInput.txt",
-  "utf8",
-  function(err, data) {
-    if (err) {
-      return console.log(err);
-    }
-    input = data.split(/[ \r\n]+/);
-    // console.log(input.length);
-    // console.log(input);
-    var c = "";
-    for (var i = 0; i < input.length; i += 2) {
-      c += parseInt(input[i]) + parseInt(input[i + 1]) + "  ";
-    }
-    // console.log(c);
-    logThis(c);
+fs.readFile(pathing, "utf8", function(err, data) {
+  if (err) {
+    return console.log(err);
   }
-);
+  input = data.split(/[ \r\n]+/);
+  // console.log(input.length);
+  // console.log(input);
+  var c = "";
+  for (var i = 0; i < input.length; i += 2) {
+    c += parseInt(input[i]) + parseInt(input[i + 1]) + "  ";
+  }
+  // console.log(c);
+  logThis(c);
+});
 
 // testing passing read value, for future calculations
 function logThis(x) {
